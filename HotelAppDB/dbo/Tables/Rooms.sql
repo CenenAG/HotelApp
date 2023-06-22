117cd3a2639836fe3db7fc75a0af01e2b9071eee
CenenAG <50300777+CenenAG@users.noreply.github.com>
2023-06-22 12:43:12 -0700

    Add project files.

---
+++ HotelAppDB/dbo/Tables/Rooms.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Rooms]
+(
+	[Id] INT NOT NULL , 
+    [RoomNumber] VARCHAR(10) NOT NULL, 
+    [RoomTypeId] INT NOT NULL, 
+    CONSTRAINT [PK_Room] PRIMARY KEY ([Id]), 
+    CONSTRAINT [FK_Rooms_RoomTypes] FOREIGN KEY (RoomTypeId) REFERENCES RoomTypes(Id) 
+)
